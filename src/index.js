776f7c386d1bbe23eed9a39ec78ba6a9bb6f2651
vladimir <lovkiy2012@gmail.com>
2020-01-26 18:11:41 +0300

    sceleton project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import React from "react";
+import { Provider } from "react-redux";
+import BookstoreService from "./services/bookstore-service";
+import ErrorBoundary from "./components/error-boundary/error-boundry";
+import { BookstoreServiceProvider } from "./components/boockstore-service-context/boockstore-service-context";
+import store from "./store";
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundary>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+        <App />
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoundary>
+  </Provider>,
+
+  document.getElementById("root")
+);
 console.log("Hellow react");
